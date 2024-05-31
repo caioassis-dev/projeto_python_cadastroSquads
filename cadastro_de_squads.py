cd346f4dd282ed18e5e41990a2c580914458156b
Caio Assis <caioassis@Users-MacBook-Pro.local>
2024-05-31 20:37:32 -0300

    refact(cadastro_squads):adicionando novas classes e atributos.

--- cadastro_de_squads.py
+++ cadastro_de_squads.py
@@ -8,3 +8,23 @@ class Squad:
         self.nome = nome
         self.devs = []
         self.techlead = techlead
+    def incluir_techlead(self, techlead):
+        self.techlead = techlead
+        
+    def incluir_dev(self, dev):
+        self.devs.append(dev)
+        
+        
+class Colaborador(Pessoa):
+    def __init__(self, nome,fone, squad=None):
+        super().__init__(nome, fone)
+        self.squad = squad
+        
+    def incluir_squad(self, squad):
+        self.squad = squad
+        
+class Dev(Colaborador):
+    def __init__(self, nome, fone,cargo, squad=None):
+        super().__init__(nome, fone, squad)
+        self.cargo = cargo
+        
